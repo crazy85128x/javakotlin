f1947501e74f2a254dcafb856f13b3bb6feb52ec
小Jason <40296790+crazy85128x@users.noreply.github.com>
2021-07-06 02:50:19 +0800

    Add files via upload

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main() {
+//    println("hello-Rose");
+//    Rose().hello()
+    val r = Rose()
+    r.hello()
+}
+
+
+class Rose{
+    fun hello(){
+        println("我在學Kotlin")
+    }
+}
